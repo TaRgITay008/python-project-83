9c4f6bda15511d3c8c616141bfe782b819c5354e
Targitay <cibrenok@bk.ru>
2026-05-08 22:29:39 +0300

    Add

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,7 @@
 """Flask application for page analyzer."""
 
 import os
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,15 +10,15 @@ load_dotenv()
 def create_app():
     """Create and configure Flask application."""
     app = Flask(__name__)
-    
+
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev-key-for-development')
     app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
-    
+
     @app.route('/')
     def index():
         """Home page."""
-        return 'Page Analyzer'
-    
+        return render_template('index.html')
+
     return app
 
 
